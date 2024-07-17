b5d2855b7e14c044cdf8e4b29adc005a07c9131e
silpako <silpako167@gmail.com>
2024-07-17 10:34:41 +0530

    commit

--- lib/1_Storages/Hive/hive_using_typeadapter/model/users.dart
+++ lib/1_Storages/Hive/hive_using_typeadapter/model/users.dart
@@ -1,4 +1,5 @@
 import 'package:hive/hive.dart';
+part 'users.g.dart';
 
 @HiveType(typeId: 0)
 class Users {
--- lib/1_Storages/Hive/hive_using_typeadapter/views/login_hive.dart
+++ lib/1_Storages/Hive/hive_using_typeadapter/views/login_hive.dart
@@ -1,1 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_frameworks/1_Storages/Hive/hive_using_typeadapter/model/users.dart';
+import 'package:flutter_frameworks/1_Storages/Hive/hive_using_typeadapter/views/reg_hive.dart';
+import 'package:get/get_core/get_core.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:hive_flutter/adapters.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox<Users>('User'); //
+  // await Hive.registerAdapter(adapter);
+}
+
+class Hive_Login extends StatelessWidget {
+  final email_controller = TextEditingController();
+  final pwd_controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Registration Page"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Login Page"),
+              const SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: email_controller,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), hintText: "User Name"),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: pwd_controller,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), hintText: "Password"),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              MaterialButton(
+                onPressed: () {},
+                shape: const StadiumBorder(),
+                color: Colors.pink,
+                child: const Text('Login Here'),
+              ),
+              TextButton(
+                  onPressed: () {
+                    Get.to(Hive_Reg());
+                  },
+                  child: Text("Not a user Register Here...!"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
